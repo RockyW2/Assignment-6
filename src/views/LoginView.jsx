52db5470a7930b06afd9f36f40c6440a8444c25d
Rocky Wang <rocky.wang2@student.tdsb.on.ca>
2024-11-21 11:39:35 -0500

    Created Login Form

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,9 +1,36 @@
 import "./LoginView.css";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function LoginView() {
-    
-    return(
-        <h1>Login View</h1>
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    function login(e) {
+        e.preventDefault();
+        if (password === "pleasegiveme100") {
+            navigate('/movies/all');
+        } else {
+            alert("Wrong password, Please Try Again");
+        }
+    }
+    return (
+
+        <div className="login-container">
+            <h1>Login View</h1>
+            <div className="form-container">
+                <form onSubmit={(e) => login(e)}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" required />
+
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" value={password} onChange={(event) => { setPassword(event.target.value) }} required />
+
+                    <button type="submit" className="login-button">Login</button>
+                </form>
+                <p className="register-link">New to Netflix? <a href="#">Register now</a></p>
+            </div>
+        </div>
     )
 }
 
